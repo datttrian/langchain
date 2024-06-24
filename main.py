8f925582c92bc96591095ed3790b516185b8bae2
datttrian <datttrian@gmail.com>
2024-06-24 15:34:20 +0000

    Mon, Jun 24, 2024, 3:34 PM +00:00

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import os
+from operator import itemgetter
 
 import openai
 from dotenv import load_dotenv
 from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from langchain_community.utilities import SQLDatabase
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
 
 from langchain.chains import create_sql_query_chain
 
-# Load environment variables
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Initialize database and language model
+
 db = SQLDatabase.from_uri("sqlite:///Chinook.db")
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-# Create the query tools
+
 execute_query = QuerySQLDataBaseTool(db=db)
 write_query = create_sql_query_chain(llm, db)
 
-# Combine the chains
-chain = write_query | execute_query
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
 
-# Invoke the chain and print the result
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+answer = answer_prompt | llm | StrOutputParser()
+chain = (
+    RunnablePassthrough.assign(query=write_query).assign(
+        result=itemgetter("query") | execute_query
+    )
+    | answer
+)
+
+
 result = chain.invoke({"question": "How many employees are there"})
 print(result)
