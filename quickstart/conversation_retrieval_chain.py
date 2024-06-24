9364c25469ed19acd78c68ba50a3c7ba38a406ab
datttrian <datttrian@gmail.com>
2024-06-24 19:59:41 +0000

    Mon, Jun 24, 2024, 7:59 PM +00:00

--- quickstart/conversation_retrieval_chain.py
+++ quickstart/conversation_retrieval_chain.py
@@ -2,14 +2,14 @@ import os
 
 import openai
 from dotenv import load_dotenv
-from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import FAISS
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.messages import HumanMessage, AIMessage
 
 # Load environment variables from a .env file
 load_dotenv()
