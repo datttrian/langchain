0ff036a81976ff33d6d0fa5836e6be49356cf226
datttrian <datttrian@gmail.com>
2024-06-24 18:53:17 +0000

    Mon, Jun 24, 2024, 6:53 PM +00:00

--- quickstart/llm_chain.py
+++ quickstart/llm_chain.py
@@ -2,9 +2,18 @@ import os
 from langchain_openai import ChatOpenAI
 import openai
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 llm = ChatOpenAI()
 result = llm.invoke("how can langsmith help with testing?")
 print(result)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a world class technical documentation writer."),
+        ("user", "{input}"),
+    ]
+)
