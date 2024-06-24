2d1452927ff135b6d4b9157e323e994cbd8cbf29
datttrian <datttrian@gmail.com>
2024-06-24 15:48:04 +0000

    Mon, Jun 24, 2024, 3:48 PM +00:00

---
+++ agent.py
@@ -0,0 +1,30 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
+from langchain_openai import ChatOpenAI
+
+from langchain_community.agent_toolkits import create_sql_agent
+
+
+# Load environment variables from .env file
+load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# Initialize SQL Database connection
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+
+# Initialize the language model
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+agent_executor = create_sql_agent(
+    llm, db=db, agent_type="openai-tools", verbose=True)
+
+result = agent_executor.invoke(
+    {
+        "input": "List the total sales per country. Which country's customers spent the most?"
+    }
+)
+
+print(result)
