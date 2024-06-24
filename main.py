9375b78c892da6a315bd7100a2cf666222279331
datttrian <datttrian@gmail.com>
2024-06-24 15:21:32 +0000

    Mon, Jun 24, 2024, 3:21 PM +00:00

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 db = SQLDatabase.from_uri("sqlite:///Chinook.db")
-print(db.dialect)
-print(db.get_usable_table_names())
-db.run("SELECT * FROM Artist LIMIT 10;")
-
-
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 chain = create_sql_query_chain(llm, db)
 response = chain.invoke({"question": "How many employees are there"})
