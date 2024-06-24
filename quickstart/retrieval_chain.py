5c0d780175afb56e9d4e68ff86ad77b9d58facc2
datttrian <datttrian@gmail.com>
2024-06-24 19:08:27 +0000

    Mon, Jun 24, 2024, 7:08 PM +00:00

--- quickstart/retrieval_chain.py
+++ quickstart/retrieval_chain.py
@@ -2,33 +2,60 @@ import os
 
 import openai
 from dotenv import load_dotenv
-from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Load environment variables from .env file
 load_dotenv()
+llm = ChatOpenAI()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Initialize the language model
-llm = ChatOpenAI()
 
-# Create the prompt template
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a world class technical documentation writer."),
-        ("user", "{input}"),
-    ]
+loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+
+docs = loader.load()
+
+
+embeddings = OpenAIEmbeddings()
+
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+
+prompt = ChatPromptTemplate.from_template(
+    """Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}"""
 )
 
-# Initialize the output parser
-output_parser = StrOutputParser()
+document_chain = create_stuff_documents_chain(llm, prompt)
 
-# Create the chain
-chain = prompt | llm | output_parser
 
-# Invoke the chain with the input and capture the output
-result = chain.invoke({"input": "how can langsmith help with testing?"})
+document_chain.invoke(
+    {
+        "input": "how can langsmith help with testing?",
+        "context": [
+            Document(page_content="langsmith can let you visualize test results")
+        ],
+    }
+)
 
-# Print the result
-print(result)
+
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
+print(response["answer"])
+
+# LangSmith offers several features that can help with testing:...
