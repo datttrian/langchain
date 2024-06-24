ecda9ef8b261963620c5cc576c6d82f2e3383c3e
datttrian <datttrian@gmail.com>
2024-06-24 15:53:03 +0000

    Mon, Jun 24, 2024, 3:53 PM +00:00

--- agent.py
+++ agent.py
@@ -8,23 +8,20 @@ from langchain_openai import ChatOpenAI
 from langchain_community.agent_toolkits import create_sql_agent
 
 
-# Load environment variables from .env file
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Initialize SQL Database connection
+
 db = SQLDatabase.from_uri("sqlite:///Chinook.db")
 
-# Initialize the language model
+
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 agent_executor = create_sql_agent(
     llm, db=db, agent_type="openai-tools", verbose=True)
 
-result = agent_executor.invoke(
+agent_executor.invoke(
     {
         "input": "Describe the playlisttrack table"
     }
 )
-
-print(result)
