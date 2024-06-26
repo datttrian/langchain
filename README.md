8b52ce50d5b671b3c408bc586e302c71ef529a48
Dat Thanh Tran <datttrian@gmail.com>
2024-06-26 18:48:23 +0000

    Wed, Jun 26, 2024, 6:48 PM +00:00

--- README.md
+++ README.md
@@ -4,12 +4,7 @@
 
 ### Basics
 
-1. [Build a Simple LLM Application with LCEL](src/llm_chain.ipynb): [serve.py](src/serve.py)
-1. Build a Chatbot
+1. [Build a Simple LLM Application with LCEL](src/serve.py)
+1. [Build a Chatbot](src/chatbot.py)
 1. Build vector stores and retrievers
-1. [Build an Agent](src/agents.ipynb): [agents.py](src/agents.py)
-
-## LangChain v0.1
-
-1. [Quickstart](src/v0.1/quickstart): [LLM Chain](src/v0.1/quickstart/llm_chain.py), [Retrieval Chain](src/v0.1/quickstart/retrieval_chain.py), [Conversation Retrieval Chain](src/v0.1/quickstart/conversation_retrieval_chain.py), [Agent](src/v0.1/quickstart/agent.py), [Serving with LangServe](src/v0.1/quickstart/serve.py)
-1. [Q&A over SQL + CSV](src/v0.1/sql): [Chain](src/v0.1/sql/chain.py), [Agent](src/v0.1/sql/agent.py)
+1. [Build an Agent](src/agents.py)
--- src/chatbot.py
+++ src/chatbot.py
@@ -1,5 +1,4 @@
 from operator import itemgetter
-
 from dotenv import load_dotenv
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
@@ -14,17 +13,26 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
 
+# Load environment variables from a .env file
 load_dotenv()
+
+# Initialize the OpenAI model with the gpt-3.5-turbo model
 model = ChatOpenAI(model="gpt-3.5-turbo")
+
+# Initialize a dictionary to store session histories
 store = {}
 
 
+# Function to retrieve or create a session history
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
+
         store[session_id] = ChatMessageHistory()
+
     return store[session_id]
 
 
+# Define a chat prompt template with system and placeholder messages
 prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -35,7 +43,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-
+# Define a trimmer to trim messages to a maximum token count
 trimmer = trim_messages(
     max_tokens=65,
     strategy="last",
@@ -45,6 +53,7 @@ trimmer = trim_messages(
     start_on="human",
 )
 
+# Define the initial set of messages
 messages = [
     SystemMessage(content="you're a good assistant"),
     HumanMessage(content="hi! I'm bob"),
@@ -60,24 +69,27 @@ messages = [
     HumanMessage(content="hi! I'm bob"),
 ]
 
-
+# Create a runnable chain that processes messages
 chain = (
     RunnablePassthrough.assign(messages=itemgetter("messages") | trimmer)
     | prompt
     | model
 )
 
-
+# Create a runnable with message history, binding the chain with the session history function
 with_message_history = RunnableWithMessageHistory(
     chain,
     get_session_history,
     input_messages_key="messages",
 )
 
+# Configuration dictionary for the session
 config = {"configurable": {"session_id": "abc16"}}
+
+# Stream responses by passing messages and language configuration to the runnable with message history
 for r in with_message_history.stream(
     {
-        "messages": [HumanMessage(content="whats my name?")],
+        "messages": messages + [HumanMessage(content="whats my name?")],
         "language": "English",
     },
     config=config,
