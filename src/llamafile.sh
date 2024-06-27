c39620fe415af8e7a114c5252efaa903eabfa404
Dat Thanh Tran <datttrian@gmail.com>
2024-06-27 21:19:08 +0000

    Thu, Jun 27, 2024, 9:19 PM +00:00

---
+++ src/llamafile.sh
@@ -0,0 +1,8 @@
+# Download a llamafile from HuggingFace
+wget https://huggingface.co/jartine/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile
+
+# Make the file executable. On Windows, instead just rename the file to end in ".exe".
+chmod +x TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile
+
+# Start the model server. Listens at http://localhost:8080 by default.
+./TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile --server --nobrowser
