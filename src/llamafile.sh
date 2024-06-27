c861b7b4ce22f925d2e902a068538e96892905bf
Dat Thanh Tran <datttrian@gmail.com>
2024-06-27 21:54:52 +0000

    Thu, Jun 27, 2024, 9:54 PM +00:00

--- src/llamafile.sh
+++ src/llamafile.sh
@@ -6,3 +6,5 @@ chmod +x TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile
 
 # Start the model server. Listens at http://localhost:8080 by default.
 ./TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile --server --nobrowser
+
+wget https://huggingface.co/caliex/all-MiniLM-L6-v2-f16.gguf/resolve/main/all-MiniLM-L6-v2-f16.gguf
