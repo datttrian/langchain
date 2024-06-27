0793827f6d2e9647601cae71c658767cd7279ec4
Dat Thanh Tran <datttrian@gmail.com>
2024-06-27 18:34:53 +0000

    Thu, Jun 27, 2024, 6:34 PM +00:00

---
+++ src/sql_qa.py
@@ -0,0 +1,79 @@
+import ast
+import re
+
+from dotenv import load_dotenv
+from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.utilities import SQLDatabase
+from langchain_community.vectorstores import FAISS
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langgraph.prebuilt import create_react_agent
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+tools = toolkit.get_tools()
+
+
+def query_as_list(db, query):
+    res = db.run(query)
+    res = [el for sub in ast.literal_eval(res) for el in sub if el]
+    res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+    return list(set(res))
+
+
+artists = query_as_list(db, "SELECT Name FROM Artist")
+albums = query_as_list(db, "SELECT Title FROM Album")
+
+
+vector_db = FAISS.from_texts(artists + albums, OpenAIEmbeddings())
+retriever = vector_db.as_retriever(search_kwargs={"k": 5})
+description = """Use to look up values to filter on. Input is an approximate spelling of the proper noun, output is \
+valid proper nouns. Use the noun most similar to the search."""
+retriever_tool = create_retriever_tool(
+    retriever,
+    name="search_proper_nouns",
+    description=description,
+)
+
+
+print(retriever_tool.invoke("Alice Chains"))
+
+
+system = """You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
+Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
+You can order the results by a relevant column to return the most interesting examples in the database.
+Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+You have access to tools for interacting with the database.
+Only use the given tools. Only use the information returned by the tools to construct your final answer.
+You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+You have access to the following tables: {table_names}
+
+If you need to filter on a proper noun, you must ALWAYS first look up the filter value using the "search_proper_nouns" tool!
+Do not try to guess at the proper name - use this function to find similar ones.""".format(
+    table_names=db.get_usable_table_names()
+)
+
+
+system_message = SystemMessage(content=system)
+
+tools.append(retriever_tool)
+
+agent = create_react_agent(llm, tools, messages_modifier=system_message)
+
+
+for s in agent.stream(
+    {"messages": [HumanMessage(content="How many albums does alis in chain have?")]}
+):
+    print(s)
+    print("----")
