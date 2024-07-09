2855deda0132ea558f9437aa5d1d569c9fc96884
datttrian <datttrian@gmail.com>
2024-07-09 19:29:14 +0000

    Tue, Jul 9, 2024, 7:29 PM +00:00

---
+++ src/summarization/refine.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_text_splitters import CharacterTextSplitter
+
+# Load environment variables from a .env file
+load_dotenv()
+
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+docs = loader.load()
+llm = ChatOpenAI(temperature=0)
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=1000, chunk_overlap=0
+)
+split_docs = text_splitter.split_documents(docs)
+
+chain = load_summarize_chain(llm, chain_type="refine")
+result = chain.invoke(split_docs)
+
+print(result["output_text"])
+
+prompt_template = """Write a concise summary of the following:
+{text}
+CONCISE SUMMARY:"""
+prompt = PromptTemplate.from_template(prompt_template)
+
+refine_template = (
+    "Your job is to produce a final summary\n"
+    "We have provided an existing summary up to a certain point: {existing_answer}\n"
+    "We have the opportunity to refine the existing summary"
+    "(only if needed) with some more context below.\n"
+    "------------\n"
+    "{text}\n"
+    "------------\n"
+    "Given the new context, refine the original summary in Italian"
+    "If the context isn't useful, return the original summary."
+)
+refine_prompt = PromptTemplate.from_template(refine_template)
+chain = load_summarize_chain(
+    llm=llm,
+    chain_type="refine",
+    question_prompt=prompt,
+    refine_prompt=refine_prompt,
+    return_intermediate_steps=True,
+    input_key="input_documents",
+    output_key="output_text",
+)
+result = chain.invoke({"input_documents": split_docs}, return_only_outputs=True)
+
+print(result["output_text"])
+
+print("\n\n".join(result["intermediate_steps"][:3]))
+
+
+def split_text(text: str):
+    return text_splitter.create_documents([text])
+
+
+summarize_document_chain = split_text | chain
