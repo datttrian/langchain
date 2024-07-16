4d90e572e9d7ff2c3e396e4b419c9396ec5aa341
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 19:41:12 +0000

    Tue, Jul 16, 2024, 7:41 PM +00:00

---
+++ src/how_to/structured_output/structured_output.py
@@ -0,0 +1,174 @@
+from typing import Optional, Union
+
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+
+llm = ChatOpenAI(model="gpt-4-0125-preview", temperature=0)
+
+
+class Joke(BaseModel):
+    """Joke to tell user."""
+
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
+    rating: Optional[int] = Field(description="How funny the joke is, from 1 to 10")
+
+
+structured_llm = llm.with_structured_output(Joke)
+
+structured_llm.invoke("Tell me a joke about cats")
+
+
+json_schema = {
+    "title": "joke",
+    "description": "Joke to tell user.",
+    "type": "object",
+    "properties": {
+        "setup": {
+            "type": "string",
+            "description": "The setup of the joke",
+        },
+        "punchline": {
+            "type": "string",
+            "description": "The punchline to the joke",
+        },
+        "rating": {
+            "type": "integer",
+            "description": "How funny the joke is, from 1 to 10",
+        },
+    },
+    "required": ["setup", "punchline"],
+}
+structured_llm = llm.with_structured_output(json_schema)
+
+structured_llm.invoke("Tell me a joke about cats")
+
+
+class ConversationalResponse(BaseModel):
+    """Respond in a conversational manner. Be kind and helpful."""
+
+    response: str = Field(description="A conversational response to the user's query")
+
+
+class Response(BaseModel):
+    output: Union[Joke, ConversationalResponse]
+
+
+structured_llm = llm.with_structured_output(Response)
+
+structured_llm.invoke("Tell me a joke about cats")
+
+
+structured_llm.invoke("How are you today?")
+
+
+structured_llm = llm.with_structured_output(json_schema)
+
+for chunk in structured_llm.stream("Tell me a joke about cats"):
+    print(chunk)
+
+
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+
+Here are some examples of jokes:
+
+example_user: Tell me a joke about planes
+example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}
+
+example_user: Tell me another joke about planes
+example_assistant: {{"setup": "Cargo", "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!", "rating": 10}}
+
+example_user: Now about caterpillars
+example_assistant: {{"setup": "Caterpillar", "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!", "rating": 5}}"""
+
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
+
+few_shot_structured_llm = prompt | structured_llm
+few_shot_structured_llm.invoke("what's something funny about woodpeckers")
+
+
+examples = [
+    HumanMessage("Tell me a joke about planes", name="example_user"),
+    AIMessage(
+        "",
+        name="example_assistant",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Why don't planes ever get tired?",
+                    "punchline": "Because they have rest wings!",
+                    "rating": 2,
+                },
+                "id": "1",
+            }
+        ],
+    ),
+    # Most tool-calling models expect a ToolMessage(s) to follow an AIMessage with tool calls.
+    ToolMessage("", tool_call_id="1"),
+    # Some models also expect an AIMessage to follow any ToolMessages,
+    # so you may need to add an AIMessage here.
+    HumanMessage("Tell me another joke about planes", name="example_user"),
+    AIMessage(
+        "",
+        name="example_assistant",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Cargo",
+                    "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!",
+                    "rating": 10,
+                },
+                "id": "2",
+            }
+        ],
+    ),
+    ToolMessage("", tool_call_id="2"),
+    HumanMessage("Now about caterpillars", name="example_user"),
+    AIMessage(
+        "",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Caterpillar",
+                    "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!",
+                    "rating": 5,
+                },
+                "id": "3",
+            }
+        ],
+    ),
+    ToolMessage("", tool_call_id="3"),
+]
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") \
+and the final punchline (the response to "<setup> who?")."""
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system), ("placeholder", "{examples}"), ("human", "{input}")]
+)
+few_shot_structured_llm = prompt | structured_llm
+few_shot_structured_llm.invoke({"input": "crocodiles", "examples": examples})
+
+
+structured_llm = llm.with_structured_output(Joke, method="json_mode")
+
+structured_llm.invoke(
+    "Tell me a joke about cats, respond in JSON with `setup` and `punchline` keys"
+)
+
+
+structured_llm = llm.with_structured_output(Joke, include_raw=True)
+
+structured_llm.invoke(
+    "Tell me a joke about cats, respond in JSON with `setup` and `punchline` keys"
+)
