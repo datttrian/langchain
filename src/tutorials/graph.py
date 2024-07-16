cebb545a8ca502416c14a363f27f1beb74fa0054
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 18:23:42 +0000

    Tue, Jul 16, 2024, 6:23 PM +00:00

---
+++ src/tutorials/graph.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+from langchain.chains import GraphCypherQAChain
+from langchain_community.graphs import Neo4jGraph
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+graph = Neo4jGraph()
+
+# Import movie information
+
+movies_query = """
+LOAD CSV WITH HEADERS FROM 
+'https://raw.githubusercontent.com/tomasonjo/blog-datasets/main/movies/movies_small.csv'
+AS row
+MERGE (m:Movie {id:row.movieId})
+SET m.released = date(row.released),
+    m.title = row.title,
+    m.imdbRating = toFloat(row.imdbRating)
+FOREACH (director in split(row.director, '|') | 
+    MERGE (p:Person {name:trim(director)})
+    MERGE (p)-[:DIRECTED]->(m))
+FOREACH (actor in split(row.actors, '|') | 
+    MERGE (p:Person {name:trim(actor)})
+    MERGE (p)-[:ACTED_IN]->(m))
+FOREACH (genre in split(row.genres, '|') | 
+    MERGE (g:Genre {name:trim(genre)})
+    MERGE (m)-[:IN_GENRE]->(g))
+"""
+
+graph.query(movies_query)
+
+
+graph.refresh_schema()
+print(graph.schema)
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+chain = GraphCypherQAChain.from_llm(graph=graph, llm=llm, verbose=True)
+response = chain.invoke({"query": "What was the cast of the Casino?"})
+response
+
+
+chain = GraphCypherQAChain.from_llm(
+    graph=graph, llm=llm, verbose=True, validate_cypher=True
+)
+response = chain.invoke({"query": "What was the cast of the Casino?"})
+response
