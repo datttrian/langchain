4de2a122548f70b6496c40aa1d7fbd1d72c871a2
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 18:29:01 +0000

    Tue, Jul 16, 2024, 6:29 PM +00:00

--- src/tutorials/graph.py
+++ src/tutorials/graph.py
@@ -3,11 +3,13 @@ from langchain.chains import GraphCypherQAChain
 from langchain_community.graphs import Neo4jGraph
 from langchain_openai import ChatOpenAI
 
+# Load environment variables from a .env file
 load_dotenv()
+
+# Initialize a connection to the Neo4j graph database
 graph = Neo4jGraph()
 
-# Import movie information
-
+# Define a query to import movie information into the Neo4j graph database
 movies_query = """
 LOAD CSV WITH HEADERS FROM 
 'https://raw.githubusercontent.com/tomasonjo/blog-datasets/main/movies/movies_small.csv'
@@ -27,21 +29,20 @@ FOREACH (genre in split(row.genres, '|') |
     MERGE (m)-[:IN_GENRE]->(g))
 """
 
+# Execute the query to import the movie information
 graph.query(movies_query)
 
-
+# Refresh the schema to ensure it is up-to-date
 graph.refresh_schema()
-print(graph.schema)
 
-
+# Initialize the OpenAI model with specified temperature and model name
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-chain = GraphCypherQAChain.from_llm(graph=graph, llm=llm, verbose=True)
-response = chain.invoke({"query": "What was the cast of the Casino?"})
-response
 
-
+# Create a GraphCypherQAChain with the graph and LLM, enabling verbose mode and Cypher query validation
 chain = GraphCypherQAChain.from_llm(
     graph=graph, llm=llm, verbose=True, validate_cypher=True
 )
+
+# Invoke the chain with a query to get the cast of the movie "Casino"
 response = chain.invoke({"query": "What was the cast of the Casino?"})
-response
+print(response)
