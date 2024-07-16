f09c4deec79b724e12240df4ba4af3d902d47936
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 20:05:17 +0000

    Tue, Jul 16, 2024, 8:05 PM +00:00

--- src/how_to/structured_output/schemas.py
+++ src/how_to/structured_output/schemas.py
@@ -1,6 +1,7 @@
 from typing import Optional, Union
 
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
 
@@ -30,5 +31,21 @@ class Response(BaseModel):
 
 structured_llm = llm.with_structured_output(Response)
 
-for chunk in structured_llm.stream("Tell me a joke about cats"):
-    print(chunk)
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+
+Here are some examples of jokes:
+
+example_user: Tell me a joke about planes
+example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}
+
+example_user: Tell me another joke about planes
+example_assistant: {{"setup": "Cargo", "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!", "rating": 10}}
+
+example_user: Now about caterpillars
+example_assistant: {{"setup": "Caterpillar", "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!", "rating": 5}}"""
+
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
+
+few_shot_structured_llm = prompt | structured_llm
+few_shot_structured_llm.invoke("what's something funny about woodpeckers")
