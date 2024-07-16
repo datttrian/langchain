1ff3e9ae908f8775b0ef7155ce74ecd339f799e9
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 20:06:24 +0000

    Tue, Jul 16, 2024, 8:06 PM +00:00

--- src/how_to/structured_output/schemas.py
+++ src/how_to/structured_output/schemas.py
@@ -1,6 +1,7 @@
 from typing import Optional, Union
 
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
@@ -31,21 +32,67 @@ class Response(BaseModel):
 
 structured_llm = llm.with_structured_output(Response)
 
+examples = [
+    HumanMessage("Tell me a joke about planes", name="example_user"),
+    AIMessage(
+        "",
+        name="example_assistant",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Why don't planes ever get tired?",
+                    "punchline": "Because they have rest wings!",
+                    "rating": 2,
+                },
+                "id": "1",
+            }
+        ],
+    ),
+    # Most tool-calling models expect a ToolMessage(s) to follow an AIMessage with tool calls.
+    ToolMessage("", tool_call_id="1"),
+    # Some models also expect an AIMessage to follow any ToolMessages,
+    # so you may need to add an AIMessage here.
+    HumanMessage("Tell me another joke about planes", name="example_user"),
+    AIMessage(
+        "",
+        name="example_assistant",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Cargo",
+                    "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!",
+                    "rating": 10,
+                },
+                "id": "2",
+            }
+        ],
+    ),
+    ToolMessage("", tool_call_id="2"),
+    HumanMessage("Now about caterpillars", name="example_user"),
+    AIMessage(
+        "",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Caterpillar",
+                    "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!",
+                    "rating": 5,
+                },
+                "id": "3",
+            }
+        ],
+    ),
+    ToolMessage("", tool_call_id="3"),
+]
 system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
-Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+Return a joke which has the setup (the response to "Who's there?") \
+and the final punchline (the response to "<setup> who?")."""
 
-Here are some examples of jokes:
-
-example_user: Tell me a joke about planes
-example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}
-
-example_user: Tell me another joke about planes
-example_assistant: {{"setup": "Cargo", "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!", "rating": 10}}
-
-example_user: Now about caterpillars
-example_assistant: {{"setup": "Caterpillar", "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!", "rating": 5}}"""
-
-prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
-
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system), ("placeholder", "{examples}"), ("human", "{input}")]
+)
 few_shot_structured_llm = prompt | structured_llm
-few_shot_structured_llm.invoke("what's something funny about woodpeckers")
+print(few_shot_structured_llm.invoke({"input": "crocodiles", "examples": examples}))
